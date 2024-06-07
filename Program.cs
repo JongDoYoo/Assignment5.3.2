2b141a9ad0de5e130b664b5ca8dd2ae23c2083bb
JongD <JongD@JONGDO>
2024-06-06 22:30:36 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment5._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalSteps = 5;
+            Console.WriteLine(MaxCombinationOfSteps(totalSteps));
+        }
+
+        static int MaxCombinationOfSteps(int steps)
+        {
+            if (steps <= 1)
+            {
+                return 1;
+            }
+
+            return MaxCombinationOfSteps(steps - 1) + MaxCombinationOfSteps(steps - 2);
+        }
+    }
+}
+
+
+
+
+
